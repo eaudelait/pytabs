e3c3d35f49850172886fadb78f081db4b4e185c4
eaudelait <44303153+eaudelait@users.noreply.github.com>
2023-11-19 20:10:01 +0100

    Create act3.py

---
+++ act3.py
@@ -0,0 +1,7 @@
+mon_tuple = (9.5, 3, 1, 4.2)
+mon_tuple[0] = 9
+
+#out: Traceback (most recent call last):
+# ERROR!
+# File "<string>", line 2, in <module>
+# TypeError: 'tuple' object does not support item assignment
