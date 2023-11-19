d760c2c0e873a586b4f4962cf9d4404ac7c13946
eaudelait <44303153+eaudelait@users.noreply.github.com>
2023-11-19 22:04:24 +0100

    Create act10.py

---
+++ act10.py
@@ -0,0 +1,5 @@
+tuple = [i for i in range(0, 100) if i % 5 == 0]
+
+#Si on l'exécute et qu'on affiche le tableau, on obtient bien le tableau des entiers multiples de 5 entre 0 et 100.
+#out: [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95]
+
