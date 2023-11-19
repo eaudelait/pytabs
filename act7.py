f571a89a17db218d88a8b7c7cea53277b742c892
eaudelait <44303153+eaudelait@users.noreply.github.com>
2023-11-19 21:43:40 +0100

    Create act7.py

---
+++ act7.py
@@ -0,0 +1,18 @@
+tab = [1, 2, 3, 4, 5, 6]
+
+print("1:" + str([i ** 2 for i in tab]))
+
+# Alternativement
+tab2 = [1, 2, 3, 4, 5, 6]
+
+def incremente_tableau(tableau):
+    car = []
+    for i in tableau:
+        car.append(i ** 2)
+    return car
+    
+print("2:" + str(incremente_tableau(tab2)))
+
+#out :
+# 1:[1, 4, 9, 16, 25, 36]
+# 2:[1, 4, 9, 16, 25, 36]
