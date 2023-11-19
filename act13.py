53152ede7deaf965e3a1fa20bffcbd1dd526f54a
eaudelait <44303153+eaudelait@users.noreply.github.com>
2023-11-19 22:24:31 +0100

    Update act13.py

--- act13.py
+++ act13.py
@@ -1,11 +1,13 @@
-notes = {"maths": 11, "francais": 13, "anglais": 16, "histoire":6}
+notes = {"maths": 11, "francais": 13, "anglais": 16, "histoire": 6}
 
 nom = None
 note = 0
 
 for m, n in notes.items():
-    if n > a:
+    if n > note:
         nom = m
         note = n
 
 print(str(note) + " " + nom)
+
+#out: 16 anglais
