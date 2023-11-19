aea691bf935f57bcd89b462620822983aedb435a
eaudelait <44303153+eaudelait@users.noreply.github.com>
2023-11-19 22:15:04 +0100

    Update act11.py

--- act11.py
+++ act11.py
@@ -3,3 +3,16 @@ matrice = []
 for i in range(1, 6):
     l = [i, i ** 2] 
     matrice.append(l)
+    
+print(matrice)
+# Pour obtenir une matrice plus lisible on peut l'afficher avec cette fonction :
+
+for i in matrice:
+    print(i)
+
+#out: [[1, 1], [2, 4], [3, 9], [4, 16], [5, 25]]
+# [1, 1]
+# [2, 4]
+# [3, 9]
+# [4, 16]
+# [5, 25]
